9c2f4e42444ae6e64d811d16bc756ea0b6b28707
Dinh Tran <mrtrandinhvn@gmail.com>
2025-12-25 00:44:53 +0700

    bump version to 2.2.2

--- publish-docker.bat
+++ publish-docker.bat
@@ -1,4 +1,4 @@
 echo Make sure that docker is running first
-docker build -t dinhgalleryapi:2.2.1 -f ./DinhGalleryApi/Dockerfile .
-docker save dinhgalleryapi:2.2.1 > dinhgalleryapi_2.2.1.tar
+docker build -t dinhgalleryapi:2.2.2 -f ./DinhGalleryApi/Dockerfile .
+docker save dinhgalleryapi:2.2.2 > dinhgalleryapi_2.2.2.tar
 pause
